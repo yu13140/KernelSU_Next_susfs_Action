23e762a64572aade82ecbba4b7ea683b6121db47
yu13140 <whmyc801@gmail.com>
2025-03-17 16:55:06 +0800

    fix

--- .github/workflows/move-folder.sh
+++ .github/workflows/move-folder.sh
@@ -1,10 +1,10 @@
 git clone -b cherry/master https://github.com/yu13140/kernel_cherry_sdm439.git
 git clone -b olive-q-oss https://github.com/yu13140/Xiaomi_Kernel_OpenSource.git
 
-cp -r "$GITHUB_WORKSPACE/kernel_cherry_sdm439/drivers/net/wireless/"* \
-       "$GITHUB_WORKSPACE/Xiaomi_Kernel_OpenSource/drivers/net/wireless/"
+cp -r "$GITHUB_WORKSPACE/Xiaomi_Kernel_OpenSource/drivers/net/wireless/" \
+       "$GITHUB_WORKSPACE/kernel_cherry_sdm439/drivers/net/wireless/"*
 
-cd "$GITHUB_WORKSPACE/Xiaomi_Kernel_OpenSource"
+cd "$GITHUB_WORKSPACE/kernel_cherry_sdm439"
 
 git add .
 git commit -m "try move"
